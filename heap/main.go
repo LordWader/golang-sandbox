f9c793c366972e2f18c62ea6364cc45f42935e00
nikolay <super.nick636@yandex.ru>
2024-04-09 01:05:24 +0300

    bugfix
    modify d-heap func

--- heap/main.go
+++ heap/main.go
@@ -9,27 +9,30 @@ type DHeapNode struct {
 type DHeap struct {
 	Width   int
 	Heap    []DHeapNode
-	Address map[*DHeapNode]int
+	Address map[int]DHeapNode
 }
 
 func NewDHeap(d int) *DHeap {
 	return &DHeap{
-		Width: d,
-		Heap:  make([]DHeapNode, 0),
+		Width:   d,
+		Heap:    make([]DHeapNode, 0),
+		Address: make(map[int]DHeapNode),
 	}
 }
 
-func (dh *DHeap) bubbleUp() {
+func (dh *DHeap) bubbleUp(startIndex int) {
 	var toCmp int
-	lastIndex := len(dh.Heap) - 1
+	lastIndex := startIndex
 	for lastIndex != 0 {
 		for i := 0; i < dh.Width; i++ {
 			if (lastIndex-i)%dh.Width == 0 {
 				toCmp = (lastIndex - i) / dh.Width
 			}
 		}
-		if dh.Heap[lastIndex].priority > dh.Heap[toCmp].priority {
+		if dh.Heap[lastIndex].priority >= dh.Heap[toCmp].priority {
 			dh.Heap[lastIndex], dh.Heap[toCmp] = dh.Heap[toCmp], dh.Heap[lastIndex]
+			dh.Address[lastIndex] = dh.Heap[toCmp]
+			dh.Address[toCmp] = dh.Heap[lastIndex]
 			lastIndex = toCmp
 		} else {
 			break
@@ -38,20 +41,35 @@ func (dh *DHeap) bubbleUp() {
 }
 
 func (dh *DHeap) Insert(elem, priority int) {
-	dh.Heap = append(dh.Heap, DHeapNode{
+	newNode := DHeapNode{
 		elem:     elem,
 		priority: priority,
-	})
-	dh.bubbleUp()
+	}
+	dh.Heap = append(dh.Heap, newNode)
+	dh.Address[len(dh.Heap)-1] = newNode
+	dh.bubbleUp(len(dh.Heap) - 1)
 }
 
 func (dh *DHeap) Top() DHeapNode {
 	carry := dh.Heap[0]
 	dh.Heap = dh.Heap[1:]
 	if len(dh.Heap) > 0 {
-		dh.bubbleUp()
+		dh.bubbleUp(len(dh.Heap) - 1)
 	}
 	return carry
+}
+
+func (dh *DHeap) Modify(priority, index int) {
+	el, ok := dh.Address[index]
+	if !ok {
+		panic("No node in heap!")
+	}
+	newNode := DHeapNode{
+		elem:     el.elem,
+		priority: priority,
+	}
+	dh.Heap[index] = newNode
+	dh.bubbleUp(index)
 }
 
 func main() {
@@ -59,6 +77,16 @@ func main() {
 	arr := []int{5, 6, 1, 2, 7}
 	for ind, el := range arr {
 		h.Insert(ind, el)
+	}
+	for len(h.Heap) > 0 {
+		fmt.Println(h.Top())
+	}
+	fmt.Println("========================")
+	for ind, el := range arr {
+		h.Insert(ind, el)
+	}
+	for i := 0; i < 3; i++ {
+		h.Modify(i*3, i)
 	}
 	for len(h.Heap) > 0 {
 		fmt.Println(h.Top())
